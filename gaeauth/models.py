595d31891561188484d25cd437d4f415db86d5de
Matt McDonald <metzen@gmail.com>
2011-03-07 11:44:41 -0500

    Add a models file to monkey patch in an index requirement on the auth.User model's password field.

---
+++ gaeauth/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+
+# Add an index for the User.password field since we'll be using it to store
+# GAE user_ids.
+for field in User._meta.local_fields:
+  if field.column == 'password':
+    field.db_index = True
+    break
