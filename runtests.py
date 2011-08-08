cbd31ef6286f7e0a94b6079079d863075abfd04e
Matt McDonald <metzen@gmail.com>
2011-08-08 09:43:39 -0400

    Add a script to simplify running tests

---
+++ runtests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+from django.conf import settings
+from django.core import management
+
+
+def runtests(args):
+  settings.configure(
+      DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+      INSTALLED_APPS=(
+          'django.contrib.auth',
+          'django.contrib.contenttypes',
+          'django.contrib.sessions',
+          'gaeauth',
+      ),
+      ROOT_URLCONF='urls')
+  management.call_command('test', *args)
+
+
+if __name__ == '__main__':
+  runtests(sys.argv[1:])
