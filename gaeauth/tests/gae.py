0ce1fb8203c6692af31fac4d61a7dbc8a2c77350
Florian Hahn <flo@fhahn.com>
2011-08-13 19:13:33 +0200

    add GaeUserApiMixin to set current user without flexmock

---
+++ gaeauth/tests/gae.py
@@ -0,0 +1,32 @@
+from google.appengine.ext import testbed
+
+
+class GaeUserApiTestMixin(object):
+    def login_user(self, email, user_id, is_admin=False):
+        self.testbed.setup_env(user_email=email or '', overwrite=True)
+        self.testbed.setup_env(user_id=user_id or '', overwrite=True)
+        self.testbed.setup_env(user_is_admin='1' if is_admin else '0',
+                               overwrite=True)
+
+    def logout_user(self):
+        self.login_user(None, None)
+
+    def setUp(self):
+        super(GaeUserApiTestMixin, self).setUp()
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_user_stub()
+
+    def tearDown(self):
+        super(GaeUserApiTestMixin, self).tearDown()
+        self.testbed.deactivate()
+
+
+class GaeOauthUserApiTestMixin(GaeUserApiTestMixin):    
+    def login_user(self, email, user_id, is_admin=False):
+        self.testbed.setup_env(oauth_email=email, overwrite=True)
+        self.testbed.setup_env(oauth_user_id=user_id or '', overwrite=True)
+        self.testbed.setup_env(oauth_auth_domain='example.com',
+                               overwrite=True)
+        self.testbed.setup_env(oauth_is_admin='1' if is_admin else '0',
+                               overwrite=True)
